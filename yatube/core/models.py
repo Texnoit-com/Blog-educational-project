847167652a9a542909976b0571e57e3aac0eef28
Maxim Bekurin <BekurinMD@yandex.ru>
2022-04-06 23:00:14 +0500

    version 1.2

--- yatube/core/models.py
+++ yatube/core/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class CreatedModel(models.Model):
     pub_date = models.DateTimeField('Дата публикации',
-                                   auto_now_add=True)
+                                    auto_now_add=True)
 
     class Meta:
         abstract = True
